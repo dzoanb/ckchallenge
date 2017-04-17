a7a0cd622aa63f27253cc1c9262a5f911b7929d8
Joanna Bednarz <dzoan@MacBook-Pro.local>
2017-04-17 19:32:37 +0200

    Git ignore and empty project.

---
+++ CKChallenge/CKChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CKChallenge
+//
+//  Created by Joanna Bednarz on 17/04/2017.
+//  Copyright © 2017 PGS Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
